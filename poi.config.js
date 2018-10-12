d90c74d68683b15e3445f4c0e984cc111269ce0f
zhangbiling <zhangbiling@zhangbilingdeMacBook-Pro.local>
2018-10-12 15:00:29 +0800

    delete the same

--- poi.config.js
+++ poi.config.js
@@ -5,7 +5,7 @@ module.exports = options => ({
         template: './src/index.html'
     },
     homepage: options.mode === 'production' ? '/sugar/' : '/',
-    port: 3000,
+    port: 5001,
     devServer: {
         proxy: 'http://192.168.10.33:3000/'
     }
